78612561657c77fbb6aa3430656a82d95407e72c
isaacwest108 <64757042+isaacwest108@users.noreply.github.com>
2023-12-17 23:25:17 +0100

    level_2.py

---
+++ Day 1 exercise/level_2.py
@@ -0,0 +1,28 @@
+"""Check the data types of the following data:
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']
+Your name
+Your family name
+Your country"""
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type("your name"))
+print(type("Your family name"))
+print(type("Your country"))
+
+
+#<class 'int'>
+#<class 'float'>
+#<class 'float'>
+#<class 'complex'>
+#<class 'list'>
+#<class 'str'>
+#<class 'str'>
+#<class 'str'>
