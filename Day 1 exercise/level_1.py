b7c5eca81c37e1d8c19491e3737b88744169294d
isaacwest108 <64757042+isaacwest108@users.noreply.github.com>
2023-12-17 23:13:39 +0100

    Add files via upload

---
+++ Day 1 exercise/level_1.py
@@ -0,0 +1,24 @@
+"""1. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//)"""
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#7
+#-1
+#12
+#3
+#0.75
+#81
+#0
